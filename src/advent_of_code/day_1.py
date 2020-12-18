a4bc833304a3be0f0568b08799eecaa1ae7c4d19
alfare <alfare@gmail.com>
2020-12-17 20:30:18 -0500

    completed day 1

---
+++ src/advent_of_code/day_1.py
@@ -0,0 +1,34 @@
+import math
+from itertools import permutations
+
+
+def get_permutations(entries: set, length: int) -> set:
+    order_dependent = set(permutations(entries, length))
+    order_independent = set()
+    for perm in order_dependent:
+        if perm == tuple(sorted(perm)):
+            order_independent.add(perm)
+    return order_independent
+
+
+def get_permutations_with_sum_limit(entries: set, length: int, sum_limit: int) -> set:
+    perms = get_permutations(entries, length)
+    return {perm for perm in perms if sum(perm) == sum_limit}
+
+
+if __name__ == '__main__':
+    with open('input_files/day_1_input.csv') as f:
+        expense_report_entries = {int(line.replace('\n', '')) for line in f.readlines()}
+    print(expense_report_entries)
+    target_combination_2 = get_permutations_with_sum_limit(
+        entries=expense_report_entries,
+        length=2,
+        sum_limit=2020
+    ).pop()
+    target_combination_3 = get_permutations_with_sum_limit(
+        entries=expense_report_entries,
+        length=3,
+        sum_limit=2020
+    ).pop()
+    print(f'2 elements = {target_combination_2}; product = {math.prod(target_combination_2)}')
+    print(f'3 elements = {target_combination_3}; product = {math.prod(target_combination_3)}')
---
+++ tests/test_day1.py
@@ -0,0 +1,18 @@
+from tests.conftest import day_1
+
+
+def test_get_permutations():
+    entries = {30, 40, 50}
+    length = 2
+    expected = {(30, 40), (30, 50), (40, 50)}
+    actual = day_1.get_permutations(entries=entries, length=length)
+    assert actual == expected
+
+
+def test_get_permutations_with_sum_limit():
+    entries = {30, 40, 50}
+    length = 2
+    sum_limit = 70
+    expected = {(30, 40)}
+    actual = day_1.get_permutations_with_sum_limit(entries=entries, length=length, sum_limit=sum_limit)
+    assert actual == expected
