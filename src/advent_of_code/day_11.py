197902a0bf56c970779f24fbfbfa8d5f443c4e54
mikealfare <alfare@gmail.com>
2020-12-21 12:22:12 -0500

    day 11 complete

---
+++ src/advent_of_code/day_11.py
@@ -0,0 +1,134 @@
+from typing import List, Tuple
+from itertools import product
+from copy import deepcopy
+
+
+filled_map = {'#': True, 'L': False, '.': False}
+can_be_filled_map = {'#': True, 'L': True, '.': False}
+
+
+def get_seat_layout_row(seat_row: str) -> List[dict]:
+    row = []
+    for each_seat in seat_row:
+        seat = {
+            'filled': filled_map[each_seat],
+            'can_be_filled': can_be_filled_map[each_seat]
+        }
+        row.append(seat)
+    return row
+
+
+def get_seat_layout(seat_rows: List[str]) -> List[List[dict]]:
+    layout = []
+    for each_row in seat_rows:
+        layout.append(get_seat_layout_row(each_row))
+    return layout
+
+
+def get_surrounding_seats_filled(seat_layout: List[List[dict]], row: int, column: int) -> int:
+    seats_to_check = [
+        (row + row_offset, column + column_offset)
+        for row_offset, column_offset in product([-1, 0, 1], [-1, 0, 1])
+        if (
+            (row_offset != 0 or column_offset != 0) and
+            row + row_offset in range(len(seat_layout)) and
+            column + column_offset in range(len(seat_layout[0]))
+        )
+    ]
+    is_seat_filled = [
+        seat_layout[check_row][check_column]['filled']
+        for check_row, check_column in seats_to_check
+    ]
+    return len([seat for seat in is_seat_filled if seat])
+
+
+def is_next_visible_seat_filled(
+        seat_layout: List[List[dict]],
+        row: int,
+        column: int,
+        row_direction: int,
+        column_direction: int
+) -> bool:
+    next_row = row + row_direction
+    next_column = column + column_direction
+    if next_row not in range(len(seat_layout)) or next_column not in range(len(seat_layout[0])):
+        return False
+    next_seat = seat_layout[next_row][next_column]
+    if next_seat['can_be_filled']:
+        return next_seat['filled']
+    return is_next_visible_seat_filled(seat_layout, next_row, next_column, row_direction, column_direction)
+
+
+def get_visible_seats_filled(seat_layout: List[List[dict]], row: int, column: int) -> int:
+    seat_count = 0
+    for row_offset, column_offset in product([-1, 0, 1], [-1, 0, 1]):
+        if row_offset != 0 or column_offset != 0:
+            if is_next_visible_seat_filled(seat_layout, row, column, row_offset, column_offset):
+                seat_count += 1
+    return seat_count
+
+
+def get_updated_seat_layout(seat_layout: List[List[dict]]) -> List[List[dict]]:
+    updated_seat_layout = deepcopy(seat_layout)
+    for row, column in product(range(len(seat_layout)), range(len(seat_layout[0]))):
+        if not seat_layout[row][column]['can_be_filled']:
+            continue
+        if not seat_layout[row][column]['filled'] and get_surrounding_seats_filled(seat_layout, row, column) == 0:
+            updated_seat_layout[row][column]['filled'] = True
+        if seat_layout[row][column]['filled'] and get_surrounding_seats_filled(seat_layout, row, column) >= 4:
+            updated_seat_layout[row][column]['filled'] = False
+    return updated_seat_layout
+
+
+def get_stable_seat_layout(seat_layout: List[List[dict]]) -> List[List[dict]]:
+    updated_seat_layout = deepcopy(seat_layout)
+    while True:
+        last_updated_seat_layout = deepcopy(updated_seat_layout)
+        updated_seat_layout = get_updated_seat_layout(updated_seat_layout)
+        if updated_seat_layout == last_updated_seat_layout:
+            return updated_seat_layout
+
+
+def get_updated_seat_layout_visible_method(seat_layout: List[List[dict]]) -> List[List[dict]]:
+    updated_seat_layout = deepcopy(seat_layout)
+    for row, column in product(range(len(seat_layout)), range(len(seat_layout[0]))):
+        if not seat_layout[row][column]['can_be_filled']:
+            continue
+        if not seat_layout[row][column]['filled'] and get_visible_seats_filled(seat_layout, row, column) == 0:
+            updated_seat_layout[row][column]['filled'] = True
+        if seat_layout[row][column]['filled'] and get_visible_seats_filled(seat_layout, row, column) >= 5:
+            updated_seat_layout[row][column]['filled'] = False
+    return updated_seat_layout
+
+
+def get_stable_seat_layout_visible_method(seat_layout: List[List[dict]]) -> List[List[dict]]:
+    updated_seat_layout = deepcopy(seat_layout)
+    while True:
+        last_updated_seat_layout = deepcopy(updated_seat_layout)
+        updated_seat_layout = get_updated_seat_layout_visible_method(updated_seat_layout)
+        if updated_seat_layout == last_updated_seat_layout:
+            return updated_seat_layout
+
+
+def get_filled_seat_count(seat_layout: List[List[dict]]) -> int:
+    filled_count = 0
+    for row, column in product(range(len(seat_layout)), range(len(seat_layout[0]))):
+        if seat_layout[row][column]['filled']:
+            filled_count += 1
+    return filled_count
+
+
+def main():
+    with open('input_files/day_11.txt') as f:
+        seat_rows = [line.strip().replace('\n', '') for line in f.readlines()]
+    seat_layout = get_seat_layout(seat_rows)
+    stable_seat_layout = get_stable_seat_layout(seat_layout)
+    stable_seat_count = get_filled_seat_count(stable_seat_layout)
+    print(f'there are {stable_seat_count} filled seats in the stable layout')
+    stable_seat_layout_visible_method = get_stable_seat_layout_visible_method(seat_layout)
+    stable_seat_count_visible_method = get_filled_seat_count(stable_seat_layout_visible_method)
+    print(f'there are {stable_seat_count_visible_method} filled seats in the visible method stable layout')
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_day_11.py
@@ -0,0 +1,175 @@
+from typing import List
+
+import pytest
+
+from tests.conftest import day_11
+
+
+sample_layout_0 = [
+    'L.LL.LL.LL',
+    'LLLLLLL.LL',
+    'L.L.L..L..',
+    'LLLL.LL.LL',
+    'L.LL.LL.LL',
+    'L.LLLLL.LL',
+    '..L.L.....',
+    'LLLLLLLLLL',
+    'L.LLLLLL.L',
+    'L.LLLLL.LL'
+]
+sample_layout_1 = [
+    '#.##.##.##',
+    '#######.##',
+    '#.#.#..#..',
+    '####.##.##',
+    '#.##.##.##',
+    '#.#####.##',
+    '..#.#.....',
+    '##########',
+    '#.######.#',
+    '#.#####.##'
+]
+sample_layout_2 = [
+    '#.LL.L#.##',
+    '#LLLLLL.L#',
+    'L.L.L..L..',
+    '#LLL.LL.L#',
+    '#.LL.LL.LL',
+    '#.LLLL#.##',
+    '..L.L.....',
+    '#LLLLLLLL#',
+    '#.LLLLLL.L',
+    '#.#LLLL.##'
+]
+sample_layout_3 = [
+    '#.##.L#.##',
+    '#L###LL.L#',
+    'L.#.#..#..',
+    '#L##.##.L#',
+    '#.##.LL.LL',
+    '#.###L#.##',
+    '..#.#.....',
+    '#L######L#',
+    '#.LL###L.L',
+    '#.#L###.##'
+]
+sample_layout_4 = [
+    '#.#L.L#.##',
+    '#LLL#LL.L#',
+    'L.L.L..#..',
+    '#LLL.##.L#',
+    '#.LL.LL.LL',
+    '#.LL#L#.##',
+    '..L.L.....',
+    '#L#LLLL#L#',
+    '#.LLLLLL.L',
+    '#.#L#L#.##'
+]
+sample_layout_5 = [
+    '#.#L.L#.##',
+    '#LLL#LL.L#',
+    'L.#.L..#..',
+    '#L##.##.L#',
+    '#.#L.LL.LL',
+    '#.#L#L#.##',
+    '..L.L.....',
+    '#L#L##L#L#',
+    '#.LLLLLL.L',
+    '#.#L#L#.##'
+]
+sample_layout_last = sample_layout_5
+
+
+@pytest.mark.parametrize('seat_row,expected', [
+    ('L.LL', [
+        {'filled': False, 'can_be_filled': True},
+        {'filled': False, 'can_be_filled': False},
+        {'filled': False, 'can_be_filled': True},
+        {'filled': False, 'can_be_filled': True},
+    ])
+])
+def test_get_seat_layout_row(seat_row: str, expected: List[dict]):
+    assert day_11.get_seat_layout_row(seat_row) == expected
+
+
+@pytest.mark.parametrize('seat_layout_human,row,column,expected', [
+    (sample_layout_2, 0, 0, 1),
+    (sample_layout_2, 1, 1, 2),
+    (sample_layout_2, 9, 9, 1),
+    (sample_layout_2, 6, 9, 3),
+])
+def test_get_surrounding_seats_filled(seat_layout_human, row, column, expected):
+    seat_layout = day_11.get_seat_layout(seat_layout_human)
+    assert day_11.get_surrounding_seats_filled(seat_layout, row, column) == expected
+
+
+@pytest.mark.parametrize('seat_layout,expected', [
+    (sample_layout_0, sample_layout_1),
+    (sample_layout_1, sample_layout_2),
+    (sample_layout_2, sample_layout_3),
+    (sample_layout_3, sample_layout_4),
+    (sample_layout_4, sample_layout_5),
+    (sample_layout_5, sample_layout_5)
+])
+def test_get_updated_seat_layout(seat_layout, expected):
+    starting_layout = day_11.get_seat_layout(seat_layout)
+    expected_layout = day_11.get_seat_layout(expected)
+    assert day_11.get_updated_seat_layout(starting_layout) == expected_layout
+
+
+@pytest.mark.parametrize('seat_layout,expected', [(sample_layout_0, sample_layout_last)])
+def test_get_stable_seat_layout(seat_layout, expected):
+    starting_layout = day_11.get_seat_layout(seat_layout)
+    expected_layout = day_11.get_seat_layout(expected)
+    assert day_11.get_stable_seat_layout(starting_layout) == expected_layout
+
+
+@pytest.mark.parametrize('seat_layout,expected', [(sample_layout_0, 37)])
+def test_get_stable_seat_count(seat_layout, expected):
+    starting_layout = day_11.get_seat_layout(seat_layout)
+    stable_layout = day_11.get_stable_seat_layout(starting_layout)
+    assert day_11.get_filled_seat_count(stable_layout) == expected
+
+
+sample_layout_visible_1 = [
+    '.......#.',
+    '...#.....',
+    '.#.......',
+    '.........',
+    '..#L....#',
+    '....#....',
+    '.........',
+    '#........',
+    '...#.....'
+]
+sample_layout_visible_2 = [
+    '.............',
+    '.L.L.#.#.#.#.',
+    '.............'
+]
+sample_layout_visible_3 = [
+    '.##.##.',
+    '#.#.#.#',
+    '##...##',
+    '...L...',
+    '##...##',
+    '#.#.#.#',
+    '.##.##.'
+]
+
+
+@pytest.mark.parametrize('seat_layout_human,row,column,expected', [
+    (sample_layout_visible_1, 4, 3, 8),
+    (sample_layout_visible_2, 1, 1, 0),
+    (sample_layout_visible_3, 3, 3, 0)
+])
+def test_get_visible_seats_filled(seat_layout_human, row, column, expected):
+    seat_layout = day_11.get_seat_layout(seat_layout_human)
+    assert day_11.get_visible_seats_filled(seat_layout, row, column) == expected
+
+
+@pytest.mark.parametrize('seat_layout,expected', [(sample_layout_0, 26)])
+def test_get_stable_seat_count_visible_method(seat_layout, expected):
+    starting_layout = day_11.get_seat_layout(seat_layout)
+    stable_layout = day_11.get_stable_seat_layout_visible_method(starting_layout)
+    assert day_11.get_filled_seat_count(stable_layout) == expected
