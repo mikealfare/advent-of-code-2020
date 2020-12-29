e9b359e48b76ccbd1323aea3b38929848e74e9c2
mikealfare <alfare@gmail.com>
2020-12-29 16:00:34 -0500

    cleaned up day 7

--- src/advent_of_code/day_07.py
+++ src/advent_of_code/day_07.py
@@ -1,56 +1,54 @@
-from typing import List, Set
+from typing import Set, Dict
+from pathlib import Path
 
 
-def convert_single_human_contained_bag_string_to_dict(human_string: str) -> dict:
-    words = human_string.strip().split(' ')
-    number = int(words[0])
-    color = ' '.join(words[1:3])
-    return {color: number}
+Bag = str
+ContainedBags = Dict[Bag, int]
+RuleSet = Dict[Bag, ContainedBags]
 
 
-def convert_composite_human_contained_bag_string_to_dict(human_string: str) -> dict:
+def get_contained_bag_counts_as_dict(human_string: str) -> ContainedBags:
     contained_bags = {}
     if human_string.strip() != 'no other bags.':
         contained_bag_strings = human_string.split(',')
         for bag_string in contained_bag_strings:
-            contained_bags.update(convert_single_human_contained_bag_string_to_dict(bag_string))
+            words = bag_string.strip().split(' ')
+            number = int(words[0])
+            color = ' '.join(words[1:3])
+            contained_bags.update({color: number})
     return contained_bags
 
 
-def convert_human_rule_to_dict(human_rule: str) -> dict:
+def convert_human_rule_to_rule(human_rule: str) -> RuleSet:
     this_bag_string, contained_bags_string = human_rule.split('contain')
     color = ' '.join(this_bag_string.strip().split(' ')[:2])
-    contained_bags = convert_composite_human_contained_bag_string_to_dict(contained_bags_string)
+    contained_bags = get_contained_bag_counts_as_dict(contained_bags_string)
     return {color: contained_bags}
 
 
-def convert_human_rules_to_ruleset_dict(human_rules: List[str]) -> dict:
+def convert_human_rules_to_ruleset(human_rules: Set[str]) -> RuleSet:
     ruleset = {}
     for rule in human_rules:
-        ruleset.update(convert_human_rule_to_dict(rule))
+        ruleset.update(convert_human_rule_to_rule(rule))
     return ruleset
 
 
-def get_bags_that_contain_given_bags_directly(bags: Set[str], ruleset: dict) -> Set[str]:
-    return {
-        color
-        for color, contained_bags in ruleset.items()
-        if bags.intersection(contained_bags)
-    }
-
-
-def get_bags_that_contain_given_bag(bag: str, ruleset: dict) -> Set[str]:
+def get_bags_that_contain_given_bag(bag: Bag, ruleset: RuleSet) -> Set[Bag]:
     bags = {bag, }
     while True:
         original_bags = len(bags)
-        new_direct_bags = get_bags_that_contain_given_bags_directly(bags=bags, ruleset=ruleset)
+        new_direct_bags = {
+            color
+            for color, contained_bags in ruleset.items()
+            if bags.intersection(contained_bags)
+        }
         bags = bags.union(new_direct_bags)
         if len(bags) == original_bags:
             bags.remove(bag)
             return bags
 
 
-def get_total_bags_within_given_bag(bag: str, ruleset: dict) -> int:
+def get_total_bags_within_given_bag(bag: Bag, ruleset: RuleSet) -> int:
     counts = []
     for child_bag, child_bag_count in ruleset[bag].items():
         bags_in_child_bag = get_total_bags_within_given_bag(bag=child_bag, ruleset=ruleset)
@@ -58,13 +56,13 @@ def get_total_bags_within_given_bag(bag: str, ruleset: dict) -> int:
     return sum(counts)
 
 
-def main():
-    with open('input_files/day_07.txt') as f:
-        human_rules = [line.replace('\n', '') for line in f.readlines()]
-    ruleset = convert_human_rules_to_ruleset_dict(human_rules=human_rules)
-    shiny_gold_bags = get_bags_that_contain_given_bag(bag='shiny gold', ruleset=ruleset)
-    print(f'{len(shiny_gold_bags)} will eventually contain a shiny gold bag: {", ".join(shiny_gold_bags)}')
-    bags_in_shiny_gold_bags = get_total_bags_within_given_bag(bag='shiny gold', ruleset=ruleset)
+def main(file_path: Path = Path(__file__).parent / 'input_files' / 'day_07.txt'):
+    with open(file_path) as f:
+        human_rules = {line.strip() for line in f.readlines()}
+    ruleset = convert_human_rules_to_ruleset(human_rules)
+    shiny_gold_bags = get_bags_that_contain_given_bag('shiny gold', ruleset)
+    print(f'{len(shiny_gold_bags)} will eventually contain a shiny gold bag')
+    bags_in_shiny_gold_bags = get_total_bags_within_given_bag('shiny gold', ruleset)
     print(f'each shiny gold bag contains {bags_in_shiny_gold_bags} bags')
 
 
--- tests/test_day_07.py
+++ tests/test_day_07.py
@@ -1,53 +1,45 @@
+from typing import Set
+
 import pytest
+
 from tests.conftest import day_07
 
 
-@pytest.mark.parametrize('rule,color', [
-    ('muted lavender bags contain 5 dull brown bags, 4 pale maroon bags, 2 drab orange bags.', 'muted lavender')
-])
-def test_parse_bag_color_from_rule(rule, color):
-    rule = day_07.convert_human_rule_to_dict(rule)
-    assert color in rule
+@pytest.fixture
+def sample_ruleset() -> day_07.RuleSet:
+    return day_07.convert_human_rules_to_ruleset({
+        'light red bags contain 1 bright white bag, 2 muted yellow bags.',
+        'dark orange bags contain 3 bright white bags, 4 muted yellow bags.',
+        'bright white bags contain 1 shiny gold bag.',
+        'muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.',
+        'shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.',
+        'dark olive bags contain 3 faded blue bags, 4 dotted black bags.',
+        'vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.',
+        'faded blue bags contain no other bags.',
+        'dotted black bags contain no other bags.'
+    })
 
 
-@pytest.mark.parametrize('rule,color,contained_bags', [
+@pytest.mark.parametrize('human_rule,expected_rule', [
     ('muted lavender bags contain 5 dull brown bags, 4 pale maroon bags, 2 drab orange bags.',
-     'muted lavender',
-     {'dull brown': 5, 'pale maroon': 4, 'drab orange': 2})
+     {'muted lavender': {'dull brown': 5, 'pale maroon': 4, 'drab orange': 2}})
 ])
-def test_parse_contained_bags_from_rule(rule, color, contained_bags):
-    rule = day_07.convert_human_rule_to_dict(rule)
-    assert rule[color] == contained_bags
+def test_convert_human_rule_to_rule(human_rule: str, expected_rule: day_07.RuleSet):
+    assert day_07.convert_human_rule_to_rule(human_rule) == expected_rule
 
 
-sample_rules = [
-    'light red bags contain 1 bright white bag, 2 muted yellow bags.',
-    'dark orange bags contain 3 bright white bags, 4 muted yellow bags.',
-    'bright white bags contain 1 shiny gold bag.',
-    'muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.',
-    'shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.',
-    'dark olive bags contain 3 faded blue bags, 4 dotted black bags.',
-    'vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.',
-    'faded blue bags contain no other bags.',
-    'dotted black bags contain no other bags.'
-]
-
-
-@pytest.mark.parametrize('bags,expected', [
-    ({'shiny gold'}, {'bright white', 'muted yellow'}),
-    ({'shiny gold', 'bright white'}, {'bright white', 'muted yellow', 'light red', 'dark orange'}),
+@pytest.mark.parametrize('bag,expected', [
+    ('shiny gold', {'bright white', 'muted yellow', 'dark orange', 'light red'}),
+    ('bright white', {'light red', 'dark orange'})
 ])
-def test_get_bags_that_contain_given_bags_directly(bags: set, expected: set):
-    ruleset = day_07.convert_human_rules_to_ruleset_dict(sample_rules)
-    assert day_07.get_bags_that_contain_given_bags_directly(bags=bags, ruleset=ruleset) == expected
+def test_get_bags_that_contain_given_bag(
+        sample_ruleset: day_07.RuleSet,
+        bag: day_07.Bag,
+        expected: Set[day_07.Bag]
+):
+    assert day_07.get_bags_that_contain_given_bag(bag, sample_ruleset) == expected
 
 
-def test_get_bags_that_contain_given_bag():
-    ruleset = day_07.convert_human_rules_to_ruleset_dict(sample_rules)
-    expected = {'bright white', 'muted yellow', 'dark orange', 'light red'}
-    assert day_07.get_bags_that_contain_given_bag('shiny gold', ruleset) == expected
-
-
-def test_get_total_bags_within_given_bag():
-    ruleset = day_07.convert_human_rules_to_ruleset_dict(sample_rules)
-    assert day_07.get_total_bags_within_given_bag('shiny gold', ruleset) == 32
+@pytest.mark.parametrize('bag,expected', [('shiny gold', 32)])
+def test_get_total_bags_within_given_bag(sample_ruleset: day_07.RuleSet, bag: day_07.Bag, expected: int):
+    assert day_07.get_total_bags_within_given_bag(bag, sample_ruleset) == expected
