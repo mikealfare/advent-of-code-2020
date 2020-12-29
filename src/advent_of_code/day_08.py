6d62c6e93fcd83ea190fe39150543af04b2b7288
mikealfare <alfare@gmail.com>
2020-12-29 17:20:24 -0500

    cleaned up day 8

--- src/advent_of_code/day_08.py
+++ src/advent_of_code/day_08.py
@@ -1,65 +1,67 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict, Union
 from copy import deepcopy
+from pathlib import Path
 
 
-def convert_human_instructions_to_instructions_dict(human_instructions: List[str]) -> dict:
+Line = int
+Instructions = Dict[Line, Dict[str, Union[int, str]]]
+
+
+def convert_human_instructions_to_instructions(human_instructions: List[str]) -> Instructions:
     result = {}
-    for i, instruction in enumerate(human_instructions):
-        mode, amount = instruction.strip().split(' ')
+    for line, human_instruction in enumerate(human_instructions):
+        mode, amount = human_instruction.strip().split(' ')
+        instruction = {'mode': mode, 'amount': int(amount), 'move': 1, 'acc': 0}
         if mode == 'nop':
-            step = {'move': 1, 'acc': 0, 'human': {'mode': 'nop', 'amount': int(amount)}}
+            pass
         elif mode == 'acc':
-            step = {'move': 1, 'acc': int(amount), 'human': {'mode': 'acc', 'amount': int(amount)}}
+            instruction.update(acc=instruction['amount'])
         elif mode == 'jmp':
-            step = {'move': int(amount), 'acc': 0, 'human': {'mode': 'jmp', 'amount': int(amount)}}
+            instruction.update(move=instruction['amount'])
         else:
             raise ValueError('unsupported instruction mode')
-        result[i] = step
+        result[line] = instruction
     return result
 
 
-def get_accumulator_when_instructions_loop_or_complete(instructions: dict) -> Tuple[int, int]:
-    executed_steps = []
-    next_step = 0
+def get_final_state(instructions: Instructions) -> Tuple[int, Line]:
     accumulator = 0
-    while True:
-        if next_step in executed_steps or next_step >= len(instructions):
-            return next_step, accumulator
-        accumulator += instructions[next_step]['acc']
-        executed_steps.append(next_step)
-        next_step += instructions[next_step]['move']
+    executed_lines = set()
+    next_line = 0
+    while next_line not in executed_lines and next_line < len(instructions):
+        accumulator += instructions[next_line]['acc']
+        executed_lines.add(next_line)
+        next_line += instructions[next_line]['move']
+    return accumulator, next_line
 
 
-def get_fixed_instructions(instructions: dict, step: int) -> dict:
-    mode = instructions[step]['human']['mode']
+def get_fixed_instructions(instructions: Instructions, line: Line) -> Instructions:
     fixed_instructions = deepcopy(instructions)
-    if mode == 'jmp':
-        fixed_instructions[step]['move'] = 1
-        return fixed_instructions
-    if mode == 'nop':
-        fixed_instructions[step]['move'] = instructions[step]['human']['amount']
-        return fixed_instructions
+    instruction = fixed_instructions[line]
+    if instruction['mode'] == 'jmp':
+        instruction['move'] = 1
+    elif instruction['mode'] == 'nop':
+        instruction['move'] = instruction['amount']
     return fixed_instructions
 
 
-def get_accumulator_by_fixing_broken_instruction(instructions: dict) -> int:
-    for step in instructions:
-        mode = instructions[step]['human']['mode']
-        if mode in ['nop', 'jmp']:
-            fixed_instructions = get_fixed_instructions(instructions, step)
-            next_step, accumulator = get_accumulator_when_instructions_loop_or_complete(fixed_instructions)
-            if next_step == len(instructions):
-                return accumulator
+def get_final_state_fixed(instructions: Instructions) -> Tuple[int, Line]:
+    for line in instructions:
+        if instructions[line]['mode'] in ['nop', 'jmp']:
+            fixed_instructions = get_fixed_instructions(instructions, line)
+            accumulator, next_line = get_final_state(fixed_instructions)
+            if next_line == len(instructions):
+                return accumulator, next_line
 
 
-def main():
-    with open('input_files/day_08.txt') as f:
-        human_instructions = [line.replace('\n', '') for line in f.readlines()]
-    instructions = convert_human_instructions_to_instructions_dict(human_instructions=human_instructions)
-    _, accumulator = get_accumulator_when_instructions_loop_or_complete(instructions)
+def main(file_path: Path = Path(__file__).parent / 'input_files' / 'day_08.txt'):
+    with open(file_path) as f:
+        human_instructions = [line.strip() for line in f.readlines()]
+    instructions = convert_human_instructions_to_instructions(human_instructions)
+    accumulator, _ = get_final_state(instructions)
     print(f'the instructions loop with accumulator {accumulator}')
-    fixed_accumulator = get_accumulator_by_fixing_broken_instruction(instructions)
-    print(f'the accumulator for the fixed instructions is {fixed_accumulator}')
+    fixed_accumulator, _ = get_final_state_fixed(instructions)
+    print(f'the fixed instructions end with accumulator {fixed_accumulator}')
 
 
 if __name__ == '__main__':
--- tests/test_day_08.py
+++ tests/test_day_08.py
@@ -1,58 +1,74 @@
+from typing import List
+
+import pytest
+
 from tests.conftest import day_08
 
 
-sample_human_instructions = [
-    'nop +0',
-    'acc +1',
-    'jmp +4',
-    'acc +3',
-    'jmp -3',
-    'acc -99',
-    'acc +1',
-    'jmp -4',
-    'acc +6'
-]
-expected_instructions = {
-    0: {'move': 1, 'acc': 0, 'human': {'mode': 'nop', 'amount': 0}},
-    1: {'move': 1, 'acc': 1, 'human': {'mode': 'acc', 'amount': 1}},
-    2: {'move': 4, 'acc': 0, 'human': {'mode': 'jmp', 'amount': 4}},
-    3: {'move': 1, 'acc': 3, 'human': {'mode': 'acc', 'amount': 3}},
-    4: {'move': -3, 'acc': 0, 'human': {'mode': 'jmp', 'amount': -3}},
-    5: {'move': 1, 'acc': -99, 'human': {'mode': 'acc', 'amount': -99}},
-    6: {'move': 1, 'acc': 1, 'human': {'mode': 'acc', 'amount': 1}},
-    7: {'move': -4, 'acc': 0, 'human': {'mode': 'jmp', 'amount': -4}},
-    8: {'move': 1, 'acc': 6, 'human': {'mode': 'acc', 'amount': 6}}
-}
-expected_fixed_instructions = {
-    0: {'move': 1, 'acc': 0, 'human': {'mode': 'nop', 'amount': 0}},
-    1: {'move': 1, 'acc': 1, 'human': {'mode': 'acc', 'amount': 1}},
-    2: {'move': 4, 'acc': 0, 'human': {'mode': 'jmp', 'amount': 4}},
-    3: {'move': 1, 'acc': 3, 'human': {'mode': 'acc', 'amount': 3}},
-    4: {'move': -3, 'acc': 0, 'human': {'mode': 'jmp', 'amount': -3}},
-    5: {'move': 1, 'acc': -99, 'human': {'mode': 'acc', 'amount': -99}},
-    6: {'move': 1, 'acc': 1, 'human': {'mode': 'acc', 'amount': 1}},
-    7: {'move': 1, 'acc': 0, 'human': {'mode': 'jmp', 'amount': -4}},  # this one
-    8: {'move': 1, 'acc': 6, 'human': {'mode': 'acc', 'amount': 6}}
-}
+@pytest.fixture
+def sample_instructions() -> day_08.Instructions:
+    instructions = ['nop +0', 'acc +1', 'jmp +4', 'acc +3', 'jmp -3', 'acc -99', 'acc +1', 'jmp -4', 'acc +6']
+    return day_08.convert_human_instructions_to_instructions(instructions)
 
 
-def test_convert_human_instructions_to_instructions_dict():
-    assert day_08.convert_human_instructions_to_instructions_dict(sample_human_instructions) == expected_instructions
+@pytest.mark.parametrize('human_instructions,expected', [
+    ([
+        'nop +0',
+        'acc +1',
+        'jmp +4',
+        'acc +3',
+        'jmp -3',
+        'acc -99',
+        'acc +1',
+        'jmp -4',
+        'acc +6'
+     ],
+     {
+        0: {'move': 1, 'acc': 0, 'mode': 'nop', 'amount': 0},
+        1: {'move': 1, 'acc': 1, 'mode': 'acc', 'amount': 1},
+        2: {'move': 4, 'acc': 0, 'mode': 'jmp', 'amount': 4},
+        3: {'move': 1, 'acc': 3, 'mode': 'acc', 'amount': 3},
+        4: {'move': -3, 'acc': 0, 'mode': 'jmp', 'amount': -3},
+        5: {'move': 1, 'acc': -99, 'mode': 'acc', 'amount': -99},
+        6: {'move': 1, 'acc': 1, 'mode': 'acc', 'amount': 1},
+        7: {'move': -4, 'acc': 0, 'mode': 'jmp', 'amount': -4},
+        8: {'move': 1, 'acc': 6, 'mode': 'acc', 'amount': 6}
+    })
+])
+def test_convert_human_instructions_to_instructions(human_instructions: List[str], expected: day_08.Instructions):
+    assert day_08.convert_human_instructions_to_instructions(human_instructions) == expected
 
 
-def test_get_accumulator_when_instructions_loop_or_complete():
-    instructions = day_08.convert_human_instructions_to_instructions_dict(sample_human_instructions)
-    assert day_08.get_accumulator_when_instructions_loop_or_complete(instructions) == (1, 5)
+@pytest.mark.parametrize('expected_acc,expected_next_line', [(5, 1)])
+def test_get_final_state(
+        sample_instructions: day_08.Instructions,
+        expected_acc: int,
+        expected_next_line: day_08.Line
+):
+    assert day_08.get_final_state(sample_instructions) == (expected_acc, expected_next_line)
 
 
-def test_get_fixed_instructions():
-    instructions = day_08.convert_human_instructions_to_instructions_dict(sample_human_instructions)
-    fixed_instructions = day_08.get_fixed_instructions(instructions, 7)
-    assert fixed_instructions == expected_fixed_instructions
-    original_instructions = day_08.convert_human_instructions_to_instructions_dict(sample_human_instructions)
-    assert instructions == original_instructions
+@pytest.mark.parametrize('line,expected', [
+    (7, {
+        0: {'move': 1, 'acc': 0, 'mode': 'nop', 'amount': 0},
+        1: {'move': 1, 'acc': 1, 'mode': 'acc', 'amount': 1},
+        2: {'move': 4, 'acc': 0, 'mode': 'jmp', 'amount': 4},
+        3: {'move': 1, 'acc': 3, 'mode': 'acc', 'amount': 3},
+        4: {'move': -3, 'acc': 0, 'mode': 'jmp', 'amount': -3},
+        5: {'move': 1, 'acc': -99, 'mode': 'acc', 'amount': -99},
+        6: {'move': 1, 'acc': 1, 'mode': 'acc', 'amount': 1},
+        7: {'move': 1, 'acc': 0, 'mode': 'jmp', 'amount': -4},
+        8: {'move': 1, 'acc': 6, 'mode': 'acc', 'amount': 6}
+    })
+])
+def test_get_fixed_instructions(
+        sample_instructions: day_08.Instructions,
+        line: day_08.Line,
+        expected: day_08.Instructions
+):
+    assert day_08.get_fixed_instructions(sample_instructions, line) == expected
 
 
-def test_get_accumulator_by_fixing_broken_instruction():
-    instructions = day_08.convert_human_instructions_to_instructions_dict(sample_human_instructions)
-    assert day_08.get_accumulator_by_fixing_broken_instruction(instructions) == 8
+@pytest.mark.parametrize('expected', [8])
+def test_get_final_state_fixed(sample_instructions: day_08.Instructions, expected: int):
+    assert day_08.get_final_state_fixed(sample_instructions) == (expected, len(sample_instructions))
