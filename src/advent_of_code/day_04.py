270119f6506003ea27093e94d2bfe9fdfadac423
mikealfare <alfare@gmail.com>
2020-12-29 13:49:44 -0500

    cleaned up day 4

--- src/advent_of_code/day_04.py
+++ src/advent_of_code/day_04.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import List
+from pathlib import Path
 
 
 @dataclass
@@ -13,27 +14,23 @@ class Passport:
     hcl: str = None
     ecl: str = None
 
+    @property
     def is_populated(self) -> bool:
-        valid = (
-            self.pid is not None and
-            self.byr is not None and
-            self.iyr is not None and
-            self.eyr is not None and
-            self.hgt is not None and
-            self.hcl is not None and
-            self.ecl is not None
-        )
-        return valid
+        return all([self.pid, self.byr, self.iyr, self.eyr, self.hgt, self.hcl, self.ecl])
 
+    @property
     def is_valid_byr(self) -> bool:
         return int(self.byr) in range(1920, 2003)
 
+    @property
     def is_valid_iyr(self) -> bool:
         return int(self.iyr) in range(2010, 2021)
 
+    @property
     def is_valid_eyr(self) -> bool:
         return int(self.eyr) in range(2020, 2031)
 
+    @property
     def is_valid_hgt(self) -> bool:
         if len(self.hgt) < 3:
             return False
@@ -45,42 +42,45 @@ class Passport:
             return scalar in range(59, 77)
         return False
 
+    @property
     def is_valid_hcl(self) -> bool:
         if len(self.hcl) != 7:
             return False
         if self.hcl[0] != '#':
             return False
-        for i in range(1, 7):
-            if self.hcl[i] not in '0123456789abcdef':
-                return False
-        return True
+        return all([char in '0123456789abcdef' for char in self.hcl[1:]])
 
+    @property
     def is_valid_ecl(self) -> bool:
         return self.ecl in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
 
+    @property
     def is_valid_pid(self) -> bool:
         if len(self.pid) != 9:
             return False
-        test_pid = int(self.pid)
+        try:
+            test_pid = int(self.pid)
+        except ValueError:
+            return False
         return True
 
+    @property
     def is_valid(self) -> bool:
-        if not self.is_populated():
+        if not self.is_populated:
             return False
-        return (
-                self.is_valid_byr() and
-                self.is_valid_eyr() and
-                self.is_valid_iyr() and
-                self.is_valid_hgt() and
-                self.is_valid_hcl() and
-                self.is_valid_ecl() and
-                self.is_valid_pid()
-        )
+        return all([
+            self.is_valid_byr,
+            self.is_valid_eyr,
+            self.is_valid_iyr,
+            self.is_valid_hgt,
+            self.is_valid_hcl,
+            self.is_valid_ecl,
+            self.is_valid_pid
+        ])
 
 
 def get_passport_from_config_string(config_string: str) -> Passport:
-    kwargs_string = config_string.replace('\n', ' ').strip()
-    kwarg_strings = kwargs_string.split(' ')
+    kwarg_strings = config_string.split(' ')
     kwargs = {}
     for kwarg_string in kwarg_strings:
         key, value = kwarg_string.split(':')
@@ -88,26 +88,27 @@ def get_passport_from_config_string(config_string: str) -> Passport:
     return Passport(**kwargs)
 
 
-def get_passports_from_file(file_path: str) -> List[Passport]:
+def get_passports_from_file(file_path: Path) -> List[Passport]:
     passports = []
-    this_passport = ''
+    this_passport = []
     with open(file_path) as f:
         for line in f.readlines():
-            if line == '\n':
-                passport = get_passport_from_config_string(this_passport)
+            if line.strip() == '':
+                passport = get_passport_from_config_string(' '.join(this_passport))
                 passports.append(passport)
-                this_passport = ''
+                this_passport = []
             else:
-                this_passport += line
-        if this_passport != '':
-            passport = get_passport_from_config_string(this_passport)
-            passports.append(passport)
+                this_passport.append(line.strip())
     return passports
 
 
-if __name__ == '__main__':
-    all_passports = get_passports_from_file('input_files/day_04.txt')
-    populated_passports = [passport for passport in all_passports if passport.is_populated()]
+def main(file_path: Path = Path(__file__).parent / 'input_files' / 'day_04.txt'):
+    all_passports = get_passports_from_file(file_path)
+    populated_passports = [passport for passport in all_passports if passport.is_populated]
     print(f'There are {len(populated_passports)} populated passports')
-    valid_passports = [passport for passport in all_passports if passport.is_valid()]
+    valid_passports = [passport for passport in all_passports if passport.is_valid]
     print(f'There are {len(valid_passports)} valid passports')
+
+
+if __name__ == '__main__':
+    main()
--- tests/test_day_04.py
+++ tests/test_day_04.py
@@ -1,46 +1,45 @@
+from pathlib import Path
+
+import pytest
+
 from tests.conftest import day_04, PACKAGE_ROOT
 
 
-sample_passport_string_full = '''ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
-byr:1937 iyr:2017 cid:147 hgt:183cm'''
-
-sample_passport_string_missing_height = '''iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
-hcl:#cfa07d byr:1929'''
+@pytest.fixture
+def sample_passport_file() -> Path:
+    return PACKAGE_ROOT / 'static' / 'day_04.txt'
 
 
-def test_get_passport_from_config_string_full():
-    passport = day_04.get_passport_from_config_string(config_string=sample_passport_string_full)
-    assert passport.pid == '860033327'
-    assert passport.cid == '147'
-    assert passport.byr == '1937'
-    assert passport.iyr == '2017'
-    assert passport.eyr == '2020'
-    assert passport.hgt == '183cm'
-    assert passport.hcl == '#fffffd'
-    assert passport.ecl == 'gry'
+@pytest.mark.parametrize('config_string,expected', [
+    ('ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm',
+     day_04.Passport(
+         pid='860033327',
+         cid='147',
+         byr='1937',
+         iyr='2017',
+         eyr='2020',
+         hgt='183cm',
+         hcl='#fffffd',
+         ecl='gry'
+     )),
+    ('iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929',
+     day_04.Passport(
+         pid='028048884',
+         cid='350',
+         byr='1929',
+         iyr='2013',
+         eyr='2023',
+         hcl='#cfa07d',
+         ecl='amb'
+     )),
+])
+def test_get_passport_from_config_string(config_string: str, expected: day_04.Passport):
+    assert day_04.get_passport_from_config_string(config_string) == expected
 
 
-def test_get_passport_from_config_string_missing_height():
-    passport = day_04.get_passport_from_config_string(config_string=sample_passport_string_missing_height)
-    assert passport.pid == '028048884'
-    assert passport.cid == '350'
-    assert passport.byr == '1929'
-    assert passport.iyr == '2013'
-    assert passport.eyr == '2023'
-    assert passport.hgt is None
-    assert passport.hcl == '#cfa07d'
-    assert passport.ecl == 'amb'
-
-
-def test_get_passports_from_file():
-    sample_file = PACKAGE_ROOT / 'static' / 'day_04.txt'
-    passports = day_04.get_passports_from_file(sample_file.absolute())
+def test_get_passports_from_file(sample_passport_file):
+    passports = day_04.get_passports_from_file(sample_passport_file)
     passport_ids = [passport.pid for passport in passports]
     assert passport_ids == ['860033327', '028048884', '760753108', '166559648']
-
-
-def test_get_populated_passport_count():
-    sample_file = PACKAGE_ROOT / 'static' / 'day_04.txt'
-    passports = day_04.get_passports_from_file(sample_file.absolute())
-    populated_passports = [passport for passport in passports if passport.is_populated()]
+    populated_passports = [passport for passport in passports if passport.is_populated]
     assert len(populated_passports) == 2
